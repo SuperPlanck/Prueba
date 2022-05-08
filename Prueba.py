3535edcd9bc9947483989de2a2da9d350076b436
SuperPlanck <102831511+SuperPlanck@users.noreply.github.com>
2022-05-07 20:02:20 -0600

    f

--- Prueba.py
+++ Prueba.py
@@ -184,17 +184,19 @@ if Temas == 'Polarización':
     st.subheader("La luz:")
     col1, col2, col3 = st.columns([1,6,1])
     with col2:
-    st.markdown("![Alt Text](https://www.liceoagb.es/quimigen/imagenes/ondaelectromagnetica1.gif)")
+        st.markdown("![Alt Text](https://www.liceoagb.es/quimigen/imagenes/ondaelectromagnetica1.gif)")
     st.write("La luz son ondas electromagneticas, visible al ojo humano,estas ondas vibran en diferentes direcciones perpendiculares en forma de propagación hasta que estas son sometidas a la polarizacion")
     st.subheader("Luz polarizada:")
     st.write("las ondas de luz vibran en un solo plano, esto se produce gracias a la ayuda de un polarizador")
+    
     col1, col2, col3 = st.columns([1,6,1])
+    
     with col2:
-    st.markdown("![Alt Text](http://www.juliohernandezfotografia.cl/wp-content/uploads/2015/10/luz-polarizador.gif)")
+        st.markdown("![Alt Text](http://www.juliohernandezfotografia.cl/wp-content/uploads/2015/10/luz-polarizador.gif)")
     st.subheader("Polarizador:")
     col1, col2, col3 = st.columns([1,6,1])
     with col2:
-    st.image("https://www.lifeder.com/wp-content/uploads/2019/04/680px-Wire-grid-polarizer2.jpg")
+        st.image("https://www.lifeder.com/wp-content/uploads/2019/04/680px-Wire-grid-polarizer2.jpg")
     botones_vector = ['Lineal','Circular','Elíptica']
     vectores = st.radio ('Tipo de polarizacion',botones_vector)
     if vectores == 'Lineal':
